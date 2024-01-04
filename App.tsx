1e93fefa59a715a9df811e0a567beec7584a1a04
Renan O Henrique <henriquebasshvf@gmail.com>
2024-01-04 13:46:40 -0300

    feat: add fonts and colors

--- App.tsx
+++ App.tsx
@@ -4,12 +4,31 @@ import {NavigationContainer} from '@react-navigation/native';
 import store from './src/redux/store';
 import Routes from './src/routes/Routes';
 
-const App: React.FC = () => (
-  <NavigationContainer>
-    <Provider store={store}>
-      <Routes />
-    </Provider>
-  </NavigationContainer>
-);
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_300Light,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter';
+
+const App = () => {
+  let [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_300Light,
+    Inter_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <></>;
+  }
+
+  return (
+    <NavigationContainer>
+      <Provider store={store}>
+        <Routes />
+      </Provider>
+    </NavigationContainer>
+  );
+};
 
 export default App;
--- src/components/Atomics/Text/Text.styles.ts
+++ src/components/Atomics/Text/Text.styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 import {css} from 'styled-components';
 import {Text as TextBase} from 'react-native';
 import {color, space, border, typography} from 'styled-system';
-import {theme, IFont, IFontSizes} from '~/styles/theme';
+import {theme, IFont, IFontSizes, IColors} from '~/styles/theme';
 
 export default {
   Text: styled(TextBase)`
@@ -16,6 +16,8 @@ export default {
       css`
         text-transform: ${textTransform};
       `}
+
+    color: ${({color}: {color: keyof IColors}) => theme.colors[color]};
 
     ${({textDecoration}) =>
       textDecoration &&
