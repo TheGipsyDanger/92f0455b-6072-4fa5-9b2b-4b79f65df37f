3f1eb220a01a46f25174e37de314cc7dc725d3b6
Renan O Henrique <henriquebasshvf@gmail.com>
2024-01-04 13:22:31 -0300

    fix: remove unused colors on theme

--- src/pages/CreateTask/index.tsx
+++ src/pages/CreateTask/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import {Div, Text} from '~/components/Atomics';
-import { ICreateTask } from '~/pages/CreateTask/CreateTask.types'
+import {ICreateTask} from '~/pages/CreateTask/CreateTask.types';
 
 export const CreateTask = (props: ICreateTask.IView) => (
   <Div flex={1} center testID={`CreateTask`}>
-    <Text color="ORANGE">{`CreateTask`}</Text>
+    <Text color="black">{`CreateTask`}</Text>
   </Div>
-)
+);
--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import {Div, Text} from '~/components/Atomics';
-import { IWelcome } from '~/pages/Welcome/Welcome.types'
+import {IWelcome} from '~/pages/Welcome/Welcome.types';
 
 export const Welcome = (props: IWelcome.IView) => (
   <Div flex={1} center testID={`Welcome`}>
-    <Text color="ORANGE">{`Welcome`}</Text>
+    <Text color="black">{`Welcome`}</Text>
   </Div>
-)
+);
