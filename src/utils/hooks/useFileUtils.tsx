f0d5c87a70dc1d9560aa93e0e4d42d6ca0f5a6b8
Renan O Henrique <henriquebasshvf@gmail.com>
2024-01-07 12:27:07 -0300

    feat: remove error on lint

--- src/utils/hooks/useFileUtils.tsx
+++ src/utils/hooks/useFileUtils.tsx
@@ -1,5 +1,6 @@
 export const useFileUtils = () => {
   const getExtensions = (value: string) => {
+    // eslint-disable-next-line no-useless-escape
     const regex = /\.([0-9a-z]+)(?:[\?#]|$)/i;
     const match = value.match(regex);
     return match ? match[1] : '';
